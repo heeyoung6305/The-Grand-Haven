3e6a898ef0ad4327cfd85affc75e37f1ef893435
heeyoung <gggmldud6305@gmail.com>
2025-10-17 03:07:10 +0900

    feat: 25. 10. 16
    header 부분의 css 및 반응형 적용

--- src/features/main/MainPage.tsx
+++ src/features/main/MainPage.tsx
@@ -4,6 +4,7 @@ import { useMainStore } from '@/providers';
 import { IMainPageProps } from '@/types';
 
 import styles from './MainPage.module.css';
+import './reset.css';
 
 const MainPage = ({ router }: IMainPageProps) => {
   const { setServiceId } = useMainStore((state) => state);
@@ -15,8 +16,16 @@ const MainPage = ({ router }: IMainPageProps) => {
   return (
     <>
       <div className={styles.main_page_body}>
-        <header>
-          <h1>The Grand Haven</h1>
+        <header className={styles.header}>
+          <nav>
+            <ul className={styles.menu_right}>
+              <li>로그인</li>
+              <li>회원가입</li>
+              <li>KR &#9660;</li>
+              <li className={styles.book_btn}>예약하기</li>
+            </ul>
+          </nav>
+          <h1 className={styles.header_title}>The Grand Haven</h1>
           <nav>
             <ul className={styles.menu_left}>
               <li>EXPERIENCE</li>
@@ -25,35 +34,30 @@ const MainPage = ({ router }: IMainPageProps) => {
               <li>ART-TAINMENT</li>
               <li>GALLERY</li>
             </ul>
-            <ul className={styles.menu_right}>
-              <li>로그인</li>
-              <li>회원가입</li>
-              <li>KR &#9660;</li>
-              <li>예약하기</li>
-            </ul>
           </nav>
         </header>
         <section className={styles.hero}>
           <div className={styles.hero_content}>
-            <div className={styles.hero_image}></div>
-            <div className={styles.hero_text}>
-              <form action="">
-                <label htmlFor="check">체크인 / 체크아웃</label>
-                <input type="date" className={'check'} />
-                {/*<a href="#" className={'check-btn'}>*/}
-                {/*  <h5>체크인 / 체크아웃</h5>*/}
-                {/*  <br />*/}
-                {/*  <h4>2025. 11. 01 ~ 2025. 11. 02</h4>*/}
-                {/*</a>*/}
-              </form>
-              <a href="#" className={styles.guest_btn}>
-                <h5>객실 및 인원</h5>
-                <br />
-                <h4>객실 1개, 투수객 1인 &#9660;</h4>
-              </a>
-              <a href="#" className={styles.search_btn}>
-                <h4>상품 검색</h4>
-              </a>
+            <div className={styles.hero_image}>
+              <div className={styles.hero_text}>
+                <div className={styles.box01}>
+                  <button className={styles.check_btn}>
+                    <h5>체크인 / 체크아웃</h5>
+                    <br />
+                    <h4>2025. 11. 01 - 2025. 11. 02</h4>
+                  </button>
+                  <button className={styles.guest_btn}>
+                    <h5>객실 및 인원</h5>
+                    <br />
+                    <h4>객실 1개, 투수객 1인 &#9660;</h4>
+                  </button>
+                </div>
+                <div className={styles.box02}>
+                  <button className={styles.search_btn}>
+                    <h4>상품 검색</h4>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </section>
@@ -67,7 +71,7 @@ const MainPage = ({ router }: IMainPageProps) => {
               Experience
             </h2>
             <h3>특별한 순간의 시작</h3>
-            <p>
+            <p className={styles.about_text}>
               그랜드 헤이븐은 도심 속에서 진정한 휴식과 감각적인 여유를 제공하는 프리미엄 라이프스타일 호텔입니다.
               현대적인 디자인과 고급스러운 인테리어가 조화를 이루는 객실과 스위트룸은 방문객에게 안락함과 특별한 경험을
               동시에 선사합니다.  저희 호텔은 단순한 숙박을 넘어, 예술과 문화, 미식과 여가를 아우르는 경험을 제공합니다.
