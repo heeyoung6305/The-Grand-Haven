64a9464b423c5cdc79fe6e592680ce748c5cf75d
Chosh93 <121915098+Chosh93@users.noreply.github.com>
2025-10-26 16:31:16 +0900

    feat: assets index.ts 추가 및 이미지 태그 사용예시 추가
    * feat: assets index.ts 추가 및 이미지 태그 사용예시 추가

---
+++ src/assets/image/index.ts
@@ -0,0 +1,23 @@
+import aboutLabel from './about_label.svg';
+import hotelLobby01 from './hotel_lobby_01.svg';
+import hotelLobby02 from './hotel_lobby_02.svg';
+import mainImg from './main_img.svg';
+import roomsGrandDeluxe01 from './rooms_grand_deluxe_suite01.svg';
+import roomsGrandDeluxe02 from './rooms_grand_deluxe_suite02.svg';
+import roomsPremiumDeluxe01 from './rooms_premium_deluxe01.svg';
+import roomsPremiumDeluxe02 from './rooms_premium_deluxe02.svg';
+import royalSuite01 from './rooms_royal_suite01.svg';
+import royalSuite02 from './rooms_royal_suite02.svg';
+
+export {
+  mainImg,
+  aboutLabel,
+  hotelLobby01,
+  hotelLobby02,
+  roomsGrandDeluxe01,
+  roomsGrandDeluxe02,
+  roomsPremiumDeluxe01,
+  roomsPremiumDeluxe02,
+  royalSuite01,
+  royalSuite02,
+};
