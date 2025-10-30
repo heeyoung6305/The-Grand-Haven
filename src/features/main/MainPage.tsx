c32a45fcb7d6632178e6c5dd08d81fe3424704ef
heeyoung <gggmldud6305@gmail.com>
2025-10-31 01:28:42 +0900

    feat: 25. 10. 30
    our facilities css 작업 95% 및 폰트 재설정

--- src/features/main/MainPage.tsx
+++ src/features/main/MainPage.tsx
@@ -9,7 +9,7 @@ import {
   roomsPremiumDeluxe01,
   roomsPremiumDeluxe02,
   safetComfort,
-  TheArtofDining,
+  TheArtOfDining,
   WineBarReserve,
 } from '@/assets';
 import { TGHHeader, TGHMenu } from '@/components';
@@ -128,57 +128,50 @@ const MainPage = ({ router }: IMainPageProps) => {
           </div>
         </section>
         <section className={styles.our_facilities}>
-          <div>
-            <div className={styles.our_facilities_images}>
-              <img src={ourFacilitiesLeft.src} alt={'시설 왼쪽 이미지'} />
-              <div>
-                <h1 className={styles.our_facilities_title}>
-                  Our
-                  <br />
-                  Faci
-                  <br />
-                  <span className={styles.lities}>Lities</span>
-                </h1>
-                <p className={styles.our_facilities_description}>
-                  다채로운 액티비티와 여유로운 휴식이 공존하는
-                  <br />
-                  그랜드 헤이븐의 특별한 시설을 즐겨보세요.
-                </p>
-              </div>
-              <img src={ourFacilitiesRight.src} alt={'시설 오른쪽 이미지'} />
+          <div className={styles.our_facilities_images}>
+            <img src={ourFacilitiesLeft.src} alt={'시설 왼쪽 이미지'} className={styles.our_facilities_left} />
+            <div>
+              <h1 className={styles.our_facilities_title}>
+                Our
+                <br />
+                Faci
+                <br />
+                <span className={styles.lities}>Lities</span>
+              </h1>
+              <p className={styles.our_facilities_description}>
+                다채로운 액티비티와 여유로운 휴식이 공존하는
+                <br />
+                그랜드 헤이븐의 특별한 시설을 즐겨보세요.
+              </p>
             </div>
+            <img src={ourFacilitiesRight.src} alt={'시설 오른쪽 이미지'} />
           </div>
           <div className={styles.our_facilities01}>
-            <h2 className={styles.our_facilities_title01}>
-              01
-              <br />
-              <br />
-              The Art <br />
-              of Dining
-            </h2>
-            <p className={styles.our_facilities_description01}>
-              고급스러운 인테리어와 세련된 분위기가 어우러진 호텔 다이닝에서 <br />
-              최상급 식자재로 완성된 미쉐린 가이드 스타 레스토랑부터 프리미엄 뷔페까지 <br />
-              셰프의 창의적인 요리와 다양한 와인, 그리고 라이브 음악이 선사하는 <br />
-              잊지 못할 미식의 순간을 경험해보세요.
-            </p>
-            <img src={TheArtofDining.src} alt={'다이닝 메인 이미지'} />
+            <div>
+              <h2 className={styles.our_facilities_title01}>
+                01
+                <br />
+                <br />
+                <br />
+                The Art <br />
+                Of Dining
+              </h2>
+              <p className={styles.our_facilities_description01}>
+                고급스러운 인테리어와 세련된 분위기가 어우러진 호텔 다이닝에서 <br />
+                최상급 식자재로 완성된 미쉐린 가이드 스타 레스토랑부터 프리미엄 뷔페까지 <br />
+                셰프의 창의적인 요리와 다양한 와인, 그리고 라이브 음악이 선사하는 <br />
+                잊지 못할 미식의 순간을 경험해보세요.
+              </p>
+            </div>
+            <img src={TheArtOfDining.src} alt={'다이닝 메인 이미지'} />
           </div>
           <div className={styles.our_facilities02}>
             <img src={GrandLounge.src} alt={'라운지 메인 이미지'} />
-            <h2>
-              02
-              <br />
-              Grand Lounge
-            </h2>
+            <h2 className={styles.our_facilities_title02}>02 Grand Lounge</h2>
           </div>
           <div className={styles.our_facilities03}>
             <img src={WineBarReserve.src} alt={'와인바 메인 이미지'} />
-            <h2>
-              03
-              <br />
-              Wine Bar “Reserve”
-            </h2>
+            <h2 className={styles.our_facilities_title03}>03 Wine Bar “Reserve”</h2>
           </div>
         </section>
         <section className={styles.safet_comfort}>
