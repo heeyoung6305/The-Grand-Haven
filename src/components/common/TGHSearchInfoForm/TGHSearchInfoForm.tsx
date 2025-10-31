340b88aba4ff49c2387ff15d52e1e1b1a8624dbc
shcho93 <shcho93@lgupluspartners.co.kr>
2025-10-31 17:30:14 +0900

    feat: 메인 상품검색 영역 퍼블리싱
    참고해서 나머지 영역도 해보세요

---
+++ src/components/common/TGHSearchInfoForm/TGHSearchInfoForm.tsx
@@ -0,0 +1,19 @@
+import { Stack, Text } from '@mantine/core';
+
+import styles from './TGHSearchInfoForm.module.css';
+
+interface ITGHSearchInfoFormProps {
+  title: string;
+  info: string;
+}
+
+const TGHSearchInfoForm = ({ title, info }: ITGHSearchInfoFormProps) => (
+  <>
+    <Stack className={styles.tgh_main_search_search_form_stack}>
+      <Text className={styles.tgh_main_search_search_form_title}>{title}</Text>
+      <Text className={styles.tgh_main_search_search_form_info}>{info}</Text>
+    </Stack>
+  </>
+);
+
+export default TGHSearchInfoForm;
--- src/components/special/TGHMainSearch/TGHMainSearch.tsx
+++ src/components/special/TGHMainSearch/TGHMainSearch.tsx
@@ -1,23 +1,21 @@
-import { Flex, Stack, Text } from '@mantine/core';
+import { BackgroundImage, Divider, Flex } from '@mantine/core';
 
-import { TGHButton } from '@/components';
+import { mainImg } from '@/assets/image';
+import { TGHButton, TGHSearchInfoForm } from '@/components';
 
 import styles from './TGHMainSearch.module.css';
 
 const TGHMainSearch = () => {
   console.log('TGHMainSearch');
   return (
-    <Flex>
-      <Stack>
-        <Text>체크인 / 체크아웃</Text>
-        <Text>2025. 11. 01 - 2025. 11. 02</Text>
-      </Stack>
-      <Stack>
-        <Text>객실 및 인원</Text>
-        <Text>객실 1개, 투수객 1인</Text>
-      </Stack>
+    <BackgroundImage className={styles.tgh_main_search_background_img} src={mainImg.src}>
+      <Flex className={styles.tgh_main_search_background_img_flex}>
+        <TGHSearchInfoForm title={'체크인 / 체크아웃'} info={'2025. 11. 01 - 2025. 11. 02'} />
+        <Divider className={styles.tgh_main_search_divider} orientation="vertical" size={3} />
+        <TGHSearchInfoForm title={'객실 및 인원'} info={'객실 1개, 투수객 1인'} />
+      </Flex>
       <TGHButton className={styles.tgh_main_search_btn} label={'상품 검색'} />
-    </Flex>
+    </BackgroundImage>
   );
 };
 
