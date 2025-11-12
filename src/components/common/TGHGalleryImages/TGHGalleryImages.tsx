cff380c2ea1e7d792f33c25b891fb8f73e9b7584
heeyoung <gggmldud6305@gmail.com>
2025-11-13 01:46:04 +0900

    feat: 25. 11. 12
    * feat: 25. 11. 12

--- src/components/common/TGHGalleryImages/TGHGalleryImages.tsx
+++ src/components/common/TGHGalleryImages/TGHGalleryImages.tsx
@@ -1,47 +1,27 @@
-import { Box, Flex, Image } from '@mantine/core';
+import { Box, Flex, Image, Text } from '@mantine/core';
 
 import { gallery01, gallery03, gallery04, gallery05 } from '@/assets';
 import { TGHGallery } from '@/components/common/TGHGallery';
 import styles from '@/components/common/TGHGalleryImages/TGHGalleryImages.module.css';
 
 const TGHGalleryImages = () => (
-  // <Box px={30}>
-  //   <Grid gutter={200} justify="space-between" align="stretch">
-  //     <Grid.Col span={{ base: 12, md: 3 }} mt={-60}>
-  //       <Image radius="none" fit="none" src={gallery01.src} />
-  //     </Grid.Col>
-  //
-  //     <Grid.Col span={{ base: 12, md: 6 }} mt={180}>
-  //       <Image radius="none" fit="none" src={gallery02.src} />
-  //     </Grid.Col>
-  //
-  //     <Grid.Col span={{ base: 12, md: 3 }} mt={-60}>
-  //       <Image radius="none" fit="none" src={gallery03.src} />
-  //     </Grid.Col>
-  //
-  //     <Grid.Col span={{ base: 12, md: 3 }} mt={-450}>
-  //       <Image radius="none" fit="none" src={gallery04.src} />
-  //     </Grid.Col>
-  //
-  //     <Grid.Col span={{ base: 12, md: 4 }}>
-  //       <Image radius="none" fit="none" src={gallery05.src} />
-  //     </Grid.Col>
-  //   </Grid>
-  // </Box>
   <Box bg={'#ede9e4'} px={30}>
     <Flex className={styles.gallery_box}>
       <Box className={styles.gallery_image_box01}>
         <Image radius="none" w="auto" fit="none" src={gallery01.src} />
-        <Image radius="none" w="auto" fit="none" src={gallery03.src} />
-        {/*<Image radius="none" w="auto" fit="none" src={gallery04.src} />*/}
+        <Image radius="none" w="auto" fit="none" src={gallery04.src} />
       </Box>
       <TGHGallery />
       <Box className={styles.gallery_image_box02}>
-        {/*<Image radius="none" w="auto" fit="none" src={gallery03.src} />*/}
-        <Image radius="none" w="auto" fit="none" src={gallery04.src} />
+        <Image radius="none" w="auto" fit="none" src={gallery03.src} />
         <Image radius="none" w="auto" fit="none" src={gallery05.src} />
       </Box>
     </Flex>
+    <Text className={styles.gallery_text_highlight}>
+      Grand Haven <span className={styles.gallery_text}>Grand__Haven©</span> Grand
+      <br />
+      Haven Grand__Haven© Grand Haven
+    </Text>
   </Box>
 );
 
--- src/components/common/TGHOFSection03/TGHOFSection03.tsx
+++ src/components/common/TGHOFSection03/TGHOFSection03.tsx
@@ -4,7 +4,7 @@ import { ofArrowButton0203, WineBarReserve } from '@/assets';
 import styles from '@/components/common/TGHOFSection03/TGHOFSection03.module.css';
 
 const TGHOFSection03 = () => (
-  <Box bg={'#fffcf6'} px={30}>
+  <Box bg={'#fffcf6'} px={30} pb={300}>
     <Text className={styles.of_section03} component="div" py={40}>
       <Image radius="none" w="auto" fit="contain" src={WineBarReserve.src} alt={'와인바 메인 이미지'} />
       <h2 className={styles.of_section_title03_1}>03</h2>
