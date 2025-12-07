922b7e8e34242a0c600f6ed2eddbe53c7ab239f5
heeyoung <gggmldud6305@gmail.com>
2025-12-07 18:53:50 +0900

    feat: feat: 25. 12.07
    * feat: feat: 25. 12.07
    
    * Merge remote-tracking branch 'origin/main' into feat/heeyoung/25.12.07

--- src/components/common/TGHHotelVisual/TGHHotelVisual.tsx
+++ src/components/common/TGHHotelVisual/TGHHotelVisual.tsx
@@ -1,4 +1,14 @@
-const TGHHotelVisual = () => (
+import { Box, Flex, Image, Text } from '@mantine/core';
 
+import { hotelVisual01 } from '@/assets';
+import styles from '@/components/common/TGHHotelVisual/TGHHotelVisual.module.css';
+
+const TGHHotelVisual = () => (
+  <Box className={styles.hotel_visual}>
+    <Flex className={styles.hotel_visual_main}>
+      <Text>Premier Standard</Text>
+      <Image radius="none" w="auto" fit="none" src={hotelVisual01.src} />
+    </Flex>
+  </Box>
 );
 export default TGHHotelVisual;
