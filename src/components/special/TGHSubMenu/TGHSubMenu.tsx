d30ec49a24f3d8577eeb735251a374b1a92c325c
Chosh93 <chosh930911@gmail.com>
2025-12-29 01:57:57 +0900

    fix: 서브메뉴 라우팅 작업
    서브메뉴 라우팅 작업

--- src/components/special/TGHSubMenu/TGHSubMenu.tsx
+++ src/components/special/TGHSubMenu/TGHSubMenu.tsx
@@ -1,16 +1,50 @@
-import { Flex, Text, Title } from '@mantine/core';
+import { Button, Flex, List } from '@mantine/core';
+import { useRouter } from 'next/navigation';
+
+import { SERVICE_MENU_DATA } from '@/assets/data';
+import { ServiceMenuEnum } from '@/enums';
+import { useMainStore } from '@/providers';
+
+import type { ITGHMenuNav } from '@/types';
 
 import styles from './TGHSubMenu.module.css';
 
-const TGHSubMenu = () => (
-  <Flex className={styles.sub_header} px={30}>
-    <Title className={styles.sub_header_title}>The Grand Haven</Title>
-    <Text>EXPERIENCE</Text>
-    <Text>HOTEL</Text>
-    <Text>DINING</Text>
-    <Text>ART COLLECTION</Text>
-    <Text>GALLERY</Text>
-  </Flex>
-);
+const TGHSubMenu = () => {
+  const { setServiceId } = useMainStore((state) => state);
+  const router = useRouter();
+  const handleLogoClick = () => {
+    router.push('/');
+  };
+
+  /**
+   * 메뉴 별 클릭 시 routing 및 스크롤 처리
+   * @param menu
+   */
+  const handleMenuClick = (menu: ITGHMenuNav) => {
+    if (
+      (menu.code === ServiceMenuEnum.HOTEL ||
+        menu.code === ServiceMenuEnum.DINING ||
+        menu.code === ServiceMenuEnum.ART_COLLECTION) &&
+      menu.link
+    ) {
+      setServiceId(menu.link);
+      router.push('/service');
+    } else {
+      console.log(menu);
+    }
+  };
+  return (
+    <Flex className={styles.sub_header} px={30}>
+      <Button variant={'transparent'} className={styles.sub_header_title} onClick={handleLogoClick}>
+        The Grand Haven
+      </Button>
+      {SERVICE_MENU_DATA.map((menu) => (
+        <List key={menu.code} className={styles.sub_header_menu_list} onClick={() => handleMenuClick(menu)}>
+          {menu.name}
+        </List>
+      ))}
+    </Flex>
+  );
+};
 
 export default TGHSubMenu;
